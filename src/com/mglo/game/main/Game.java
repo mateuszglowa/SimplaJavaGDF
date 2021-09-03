c8877e0ff8e3d8f5f2436f93560c8d430f1fb34f
Mateusz Glowa <glowa.mateusz@gmail.com>
2021-09-03 11:42:39 +0200

    implementing LoadState with Game class

--- src/com/mglo/game/main/Game.java
+++ src/com/mglo/game/main/Game.java
@@ -1,5 +1,8 @@
 package com.mglo.game.main;
 
+import com.mglo.game.state.LoadState;
+import com.mglo.game.state.State;
+
 import javax.swing.JPanel;
 import java.awt.*;
 
@@ -12,6 +15,7 @@ public class Game extends JPanel {
 
     private Thread gameThread;
     private volatile boolean running;
+    private volatile State currentState;
 
     public Game(int gameWidth, int gameHeight){
         this.gameWidth = gameWidth;
@@ -20,5 +24,17 @@ public class Game extends JPanel {
         setBackground(Color.BLACK);
         setFocusable(true);
         requestFocus();
+    }
+
+    public void setCurrentState(State newState){
+        System.gc();
+        newState.init();
+        currentState = newState;
+    }
+
+    @Override
+    public void addNotify() {
+        super.addNotify();
+        setCurrentState(new LoadState());
     }
 }
