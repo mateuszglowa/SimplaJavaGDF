6372ed879eb22ebafff436b2386770d8b8723853
Mateusz Glowa <glowa.mateusz@gmail.com>
2021-09-03 11:26:29 +0200

    Adding LoadState class

---
+++ src/com/mglo/game/state/LoadState.java
@@ -0,0 +1,41 @@
+package com.mglo.game.state;
+
+import com.mglo.game.main.Resources;
+
+import javax.annotation.Resource;
+import java.awt.*;
+import java.awt.event.KeyEvent;
+import java.awt.event.MouseEvent;
+
+public class LoadState extends State {
+    @Override
+    public void init() {
+        Resources.load();
+        System.out.print("Resources Loaded Successfully");
+    }
+
+    @Override
+    public void update() {
+
+    }
+
+    @Override
+    public void render(Graphics G) {
+
+    }
+
+    @Override
+    public void onClick(MouseEvent e) {
+
+    }
+
+    @Override
+    public void onKeyPress(KeyEvent e) {
+
+    }
+
+    @Override
+    public void onKeyRelease(KeyEvent e) {
+
+    }
+}
--- src/com/mglo/game/state/State.java
+++ src/com/mglo/game/state/State.java
@@ -1,4 +1,21 @@
 package com.mglo.game.state;
 
+import java.awt.Graphics;
+
+import java.awt.event.KeyEvent;
+import java.awt.event.MouseEvent;
+
 public abstract class State {
+
+    public abstract void init();
+
+    public abstract void update();
+
+    public abstract void render(Graphics G);
+
+    public abstract void onClick(MouseEvent e);
+
+    public abstract void onKeyPress(KeyEvent e);
+
+    public abstract void onKeyRelease(KeyEvent e);
 }
