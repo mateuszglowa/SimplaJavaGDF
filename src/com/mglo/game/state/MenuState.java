ecaa2926b077302cf814e50556f15adc08dc299a
Mateusz Glowa <glowa.mateusz@gmail.com>
2021-09-03 14:24:45 +0200

    added MenuState class and setCurrentState in LoadState

--- src/com/mglo/game/state/MenuState.java
+++ src/com/mglo/game/state/MenuState.java
@@ -2,40 +2,39 @@ package com.mglo.game.state;
 
 import com.mglo.game.main.Resources;
 
-import javax.annotation.Resource;
-import java.awt.*;
+import java.awt.Graphics;
 import java.awt.event.KeyEvent;
 import java.awt.event.MouseEvent;
 
-public class LoadState extends State {
+public class MenuState extends State {
     @Override
     public void init() {
-        Resources.load();
-        System.out.print("Resources Loaded Successfully");
+        System.out.println("Entered MenuState");
     }
 
     @Override
     public void update() {
-        setCurrentState(new MenuState()); //This is the new line!
+        //Do nothing
     }
 
     @Override
-    public void render(Graphics G) {
-
+    public void render(Graphics g) {
+        //Draws Resources.welcome to the screen at x = 0, y = 0
+        g.drawImage(Resources.welcome, 0, 0, null);
     }
 
     @Override
     public void onClick(MouseEvent e) {
-
+        //TODO
     }
 
     @Override
     public void onKeyPress(KeyEvent e) {
-
+        //Intentionally ignored
     }
 
     @Override
     public void onKeyRelease(KeyEvent e) {
-
+        //Intentionally ignored
     }
 }
--- src/com/mglo/game/state/State.java
+++ src/com/mglo/game/state/State.java
@@ -1,5 +1,7 @@
 package com.mglo.game.state;
 
+import com.mglo.game.main.GameMain;
+
 import java.awt.Graphics;
 
 import java.awt.event.KeyEvent;
@@ -18,4 +20,8 @@ public abstract class State {
     public abstract void onKeyPress(KeyEvent e);
 
     public abstract void onKeyRelease(KeyEvent e);
+
+    public void setCurrentState(State newState){
+        GameMain.sGame.setCurrentState(newState);
+    }
 }
