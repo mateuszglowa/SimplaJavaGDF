a5b6cd532023844775b18c8e7726bc707641479b
Mateusz Glowa <glowa.mateusz@gmail.com>
2021-09-03 10:07:45 +0200

    adding GameMain as Main init class and removing Main.java (dummy class)

--- src/com/mglo/game/main/GameMain.java
+++ src/com/mglo/game/main/GameMain.java
@@ -1,4 +1,17 @@
 package com.mglo.game.main;
 
+import javax.swing.*;
+
 public class GameMain {
+    private static final String GAME_TITLE = "Java Game Development Framework (Chapter 4)";
+    public static final int GAME_WIDTH = 800;
+    public static final int GAME_HEIGHT = 450;
+    public static Game game;
+
+    public static void main(String[] args){
+        JFrame frame = new JFrame(GAME_TITLE);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setResizable(false); //prevents manual resizing of window
+        frame.setVisible(true);
+    }
 }
