a3b9d606b367c2c15019921cad76bfa388af007b
Mateusz Glowa <glowa.mateusz@gmail.com>
2021-09-03 10:14:52 +0200

    adding Height and Wifth in Game class plus constructor

--- src/com/mglo/game/main/Game.java
+++ src/com/mglo/game/main/Game.java
@@ -1,4 +1,24 @@
 package com.mglo.game.main;
 
-public class Game {
+import javax.swing.JPanel;
+import java.awt.*;
+
+@SuppressWarnings("serial")
+
+public class Game extends JPanel {
+    private int gameWidth;
+    private int gameHeight;
+    private Image gameImage;
+
+    private Thread gameThread;
+    private volatile boolean running;
+
+    public Game(int gameWidth, int gameHeight){
+        this.gameWidth = gameWidth;
+        this.gameHeight = gameHeight;
+        setPreferredSize(new Dimension(gameWidth, gameHeight));
+        setBackground(Color.BLACK);
+        setFocusable(true);
+        requestFocus();
+    }
 }
