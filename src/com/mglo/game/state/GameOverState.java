7908ba6b581502d8c66d9e60683b11596600d81c
Mateusz Glowa <glowa.mateusz@gmail.com>
2021-11-20 14:12:17 +0100

    Added GameOVerState

---
+++ src/com/mglo/game/state/GameOverState.java
@@ -0,0 +1,59 @@
+package com.mglo.game.state;
+
+import com.mglo.game.main.GameMain;
+
+import java.awt.*;
+import java.awt.event.KeyEvent;
+import java.awt.event.MouseEvent;
+
+public class GameOverState extends State {
+
+    private String playerScore;
+    private Font font;
+
+    public GameOverState(String playerScore) {
+        this.playerScore = playerScore + "";
+        font = new Font("SansSerif", Font.BOLD,50);
+    }
+
+    @Override
+    public void init() {
+
+    }
+
+    @Override
+    public void update() {
+
+    }
+
+    @Override
+    public void update(float delta) {
+
+    }
+
+    @Override
+    public void render(Graphics g) {
+        g.setColor(Color.ORANGE);
+        g.fillRect(0,0, GameMain.GAME_WIDTH, GameMain.GAME_HEIGHT);
+        g.setColor(Color.DARK_GRAY);
+        g.setFont(font);
+        g.drawString("Game Over", 257,175);
+        g.drawString(playerScore, 385,250);
+        g.drawString("Press any key.", 240, 350);
+    }
+
+    @Override
+    public void onClick(MouseEvent e) {
+
+    }
+
+    @Override
+    public void onKeyPress(KeyEvent e) {
+
+    }
+
+    @Override
+    public void onKeyRelease(KeyEvent e) {
+
+    }
+}
