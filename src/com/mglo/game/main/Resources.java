5818eaa45b64ad8ca221f4348b923b9a893ddadf
Mateusz Glowa <glowa.mateusz@gmail.com>
2021-11-20 09:55:48 +0100

    Added 16 resources and Cloud.class

--- src/com/mglo/game/main/Resources.java
+++ src/com/mglo/game/main/Resources.java
@@ -10,11 +10,25 @@ import java.net.URL;
 
 public class Resources {
 
-    public static BufferedImage welcome, iconimage;
+    public static BufferedImage welcome, iconimage, block, cloud1, cloud2,
+                    duck, grass, jump, run1, run2, run3, run4, run5, selector;
+    public static AudioClip hit, onJump;
 
     public static void load(){
         welcome = loadImage("welcome.png");
         iconimage = loadImage("iconimage.png");
+        block = loadImage("block.png");
+        cloud1 = loadImage("cloud1.png");
+        cloud2 = loadImage("cloud2.png");
+        duck = loadImage("duck.png");
+        grass = loadImage("grass.png");
+        jump = loadImage("jump.png");
+        run1 = loadImage("run1.png");
+        run2 = loadImage("run2.png");
+        run3 = loadImage("run3.png");
+        run4 = loadImage("run4.png");
+        run5 = loadImage("run5.png");
+        selector = loadImage("selector.png");
     }
 
     public static AudioClip loadSound(String fileName){
